7fe9b39e323a6b1a85c7eeeedc45bb051cb9d60f
Caleb James <calebsjames@gmail.com>
2021-04-28 14:43:13 -0500

    feat: add and edit form

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom"
 import { PostList } from "./posts/PostList"
 import { PostProvider } from "./posts/PostProvider"
 import { PostDetail } from "./posts/PostDetails"
+import { PostForm } from "./posts/PostForm"
 
 
 export const ApplicationViews = () => {
@@ -17,6 +18,12 @@ export const ApplicationViews = () => {
             </Route>
             <Route exact path="/posts/detail/:postId(\d+)">
                 <PostDetail />
+            </Route>
+            <Route exact path="/posts/edit/:postId(\d+)">
+                <PostForm />
+            </Route>
+            <Route exact path="/posts/create">
+                <PostForm />
             </Route>
         </PostProvider>
         </main>
