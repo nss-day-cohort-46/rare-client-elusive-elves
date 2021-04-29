26bbbd33649a047b7ef57cb7c46a976467727c94
nathanRomad <nathan.a.hamilton@gmail.com>
2021-04-29 09:42:08 -0500

    build: category list

--- src/components/category/CategoryList.js
+++ src/components/category/CategoryList.js
@@ -8,13 +8,19 @@ import { CategoryCard } from "./CategoryCard"
 
 
 export const CategoryList = () => {
-    const { getCategories, addCategory, getCategoryById, deleteCategory, updateCategory} = useContext(CategoryContext)
+    const { getCategories, addCategory, getCategoryById, deleteCategory, updateCategory, searchTerms} = useContext(CategoryContext)
 
     // useState to return filtered categories
     const [ filteredcategories, setFiltered ] = useState([])
     const history = useHistory()
   
     // Initialization effect hook -> Go get Category data
-    useEffect(()=>{
+    useEffect(() => {
       getcategories()
     }, [])
+
+    return (
+      <section className="CategoryList">
+        <h1> Category List </h1>
+        <button onClick={() => history.push("/posts/create")}> New Category </button>
+      </section>    )
