f263b2491e033b9478067db80857e3f211d29fd1
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-29 13:22:21 -0500

    build: building my post list, routing through app views, added my posts to navbar

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { PostList } from "./posts/PostList"
+import { MyPostsList } from "./posts/MyPostsList"
 import { PostProvider } from "./posts/PostProvider"
 import { PostDetail } from "./posts/PostDetails"
 import { PostForm } from "./posts/PostForm"
@@ -19,6 +20,9 @@ export const ApplicationViews = () => {
             <UserProvider>
                 <Route exact path="/">
                     <PostList />
+                </Route>
+                <Route exact path="/mypostlist">
+                    <MyPostsList />
                 </Route>
                 <Route exact path="/posts/detail/:postId(\d+)">
                     <PostDetail />
---
+++ src/components/posts/MyPostsList.js
@@ -0,0 +1,60 @@
+import React, { useState, useContext, useEffect } from "react"
+import { PostContext } from "./PostProvider"
+
+import { PostCard } from "./PostCard"
+import "./Post.css"
+import { useHistory } from "react-router-dom"
+import { UserContext } from "../users/UserProvider"
+
+//TODO: make a list so only user posts are visible
+export const MyPostsList = () => {
+    const { posts, searchTerms, getPostsByUserId } = useContext(PostContext)
+    const { getUsers, users } = useContext(UserContext)
+
+    // useState to return filtered posts
+    const [filteredPosts, setFiltered] = useState([])
+    const history = useHistory()
+
+    const rareUser = +sessionStorage.getItem('rare_user_id')
+
+    // Initialization effect hook -> Go get post data
+    useEffect(() => {
+        getPostsByUserId(rareUser)
+            .then(setFiltered)
+    }, [])
+
+    //search user posts
+    useEffect(() => {
+        if (searchTerms !== "") {
+            // If the search field is not blank, display matching posts
+            const subset = posts.filter(post => post.title.toLowerCase().includes(searchTerms.toLowerCase))
+            setFiltered(subset)
+        } else {
+            // If the search field is blank, display all posts
+            setFiltered(posts)
+        }
+    }, [searchTerms, posts])
+
+    return (
+        <>
+            <h1>My Posts</h1>
+
+            <button onClick={() => history.push("/posts/create")}>
+                New Post
+            </button>
+            <div className="posts">
+                {
+                    filteredPosts.map(postObject => {
+                        const author = users.find(u => parseInt(u.id) === parseInt(posts.user_id))
+
+                        return <PostCard key={postObject.id} postInstance={postObject}
+                            postAuthor={author}
+                        />
+                    })
+                }
+            </div>
+        </>
+    )
+
+
+}
