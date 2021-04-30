cd8954f8ff18578ab0acac8154204851714c32a3
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-30 13:32:56 -0500

    feat: adding tags to home page for filtering posts

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -6,6 +6,7 @@ import "./Post.css"
 import { useHistory } from "react-router-dom"
 import { UserContext } from "../users/UserProvider"
 import { CategoryContext } from "../category/CategoryProvider"
+import { TagContext } from "../tags/TagsProvider"
 
 
 
@@ -13,18 +14,26 @@ export const PostList = () => {
     const { getPosts, posts, searchTerms } = useContext(PostContext)
     const { getUsers, users } = useContext(UserContext)
     const { getCategories, categories } = useContext(CategoryContext)
+    const { tags, getTags, getPostTags } = useContext(TagContext)
 
     // useState to return filtered posts
     const [ filteredPosts, setFiltered ] = useState([])
     const history = useHistory()
   
+    
+    //state variable for post-tags
+    const [postTags, setPostTags] = useState([])
+
     // Initialization effect hook -> Go get post data
     useEffect(()=>{
       getPosts()
       getUsers()
       getCategories()
+      getTags()
+      getPostTags().then(setPostTags)
     }, [])
   
+
     useEffect(() => {
       if (searchTerms !== "") {
         // If the search field is not blank, display matching posts
@@ -37,12 +46,24 @@ export const PostList = () => {
     }, [searchTerms, posts])
 
 
-
     const postsSorted = posts.sort(
       (currentPost, nextPost) =>
           Date.parse(nextPost.publication_date) - Date.parse(currentPost.publication_date)
     )
-  
+    setFiltered(postsSorted)
+
+
+    //filter by tags
+    const handleTagFilter = (e) => {
+      tagId = parseInt(e.target.id)
+      postsWithThisTag = postTags.filter(pt => pt.tag_id === tagId)
+      matchingPosts = postsSorted.filter(post => post.id === postsWithThisTag.post_id)
+      setFiltered(matchingPosts)
+    }
+
+
+
+
       return (
         <>
             <h1>Posts</h1>
@@ -52,7 +73,7 @@ export const PostList = () => {
             </button>
             <div className="posts">
                 {
-                    postsSorted.map(postObject => {
+                    filteredPosts.map(postObject => {
                       
                         const author = users.find(u => parseInt(u.id) === parseInt(postObject.user_id))
                         const category = categories.find(c => parseInt(c.id) === parseInt(postObject.category_id))
@@ -64,6 +85,11 @@ export const PostList = () => {
                     })
                 }
             </div>
+            <aside className="tags">
+                {tags.map(tag => {
+                  <button id={`tagBtn--${tag.id}`} onClick={}>{tag.label}</button>
+                })}
+            </aside>
         </>
     )
 }
